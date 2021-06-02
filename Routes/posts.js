34062837a065042cff9984df7e1954c385a1ecf6
Sushilkc855 <susk-c020418@student.jenseneducation.se>
2021-06-02 21:39:50 +0200

    updated

--- Routes/posts.js
+++ Routes/posts.js
@@ -2,32 +2,33 @@ const express = require('express');
 const router = express.Router(); // the post route. 
 const Post = require('../models/Post');
 
+// First get all the posts in DB and respond with all these posts. when a GET request is made to the root route, /posts.
 router.get('/', async (req, res) =>{
     try{
-        const posts = await Post.find(); // to get back all the data
+        const posts = await Post.find(); // to get back all the data. Awaite is a promise. The code won't go further if until it finds the data in the database. 
         res.json(posts);
-    }catch(err){
+    }catch(err){ // if we don't get anything/posts. Respond with an err message 
         res.json({message:err});
     }
 });
 
-
-router.post('/', async(req, res) => {
-    const post = new Post({ // New post from the model
+//respond to POST request on the route (/posts). 
+router.post('/', async (req, res) => { // Function which have async have A special key
+    const post = new Post({ // New post from the model. We make a new schema
         title: req.body.title,
         pris: req.body.pris,
         image: req.body.image
     });
 
     try{
-        const savedPost = await post.save(); // Saving the data to our database    
+        const savedPost = await post.save(); // Saving the data to our database. .save() moongose package.  
         res.json(savedPost);
     }catch(err){
         res.json({message: err});
     }
 });
 
-module.exports = router;
+module.exports = router; //
 
 
 
--- api.js
+++ api.js
@@ -1,20 +1,23 @@
-const express = require('express'); // gets the package into this file 
-const app = express(); // we execute
+const express = require('express'); // gets the package into this file.Express is an Routing and middleware web framework
+const app = express(); // creates a new express application for you
 const mongoose = require('mongoose');
 require('dotenv/config');
+//This is a method inbuilt in express to recognize the incoming Request Object as a JSON Object. This method is called as a middleware in your application using the code:
+app.use(express.json());
+//Middleware: It is those methods/functions/operations that are called BETWEEN processing the Request and sending the Response in your application method.
 
-app.use(express.json()); // every time we hit req this body parser runs
-//Middleware: A function that execute routes are being hite
-// Import routes
-const postsRoute = require('./Routes/posts'); //Inporting the folder and the file
-app.use('/posts', postsRoute);//Everytime we go to /posts these postsRoute is gonna run
+const postsRoute = require('./Routes/posts'); //Inporting the file in the folder
+//This line of code shows a middleware function mounted on the /posts. The function is executed for any type of HTTP request on the /posts
+app.use('/posts', postsRoute);//Everytime we go to /posts these postsRoute is gonna run. we can use app.use() to specify middleware as the callback function. mostly used to set up middleware for your application.
 
 
-
-// Routes 
-app.get('/', (req, res)=> { // Post: to send message. Delete, get, patch(update)
+// respond with "hello there" when a GET request is made to the homepage
+// GET method route. testing. 
+app.get('/', (req, res)=> { // Post: to send message. Delete, get, patch(update). To handle get requests
     res.send('Hello there')// '/' Route we wanna go to. Just localHost
 });
+
+
 
 //connect to DataBase
 mongoose.connect(process.env.DB_CONNECTION,  //For securty reason
